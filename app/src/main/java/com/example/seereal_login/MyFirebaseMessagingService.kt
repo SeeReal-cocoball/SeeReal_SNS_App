269cf776fceebb9b19571f257c41c994c9f14e24
ryugeumjung <fbgjung@gmail.com>
2023-05-16 00:59:40 +0900

    test

--- app/src/main/java/com/example/seereal_login/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/seereal_login/MyFirebaseMessagingService.kt
@@ -18,6 +18,7 @@ class MyFirebaseMessagingService : AppCompatActivity() {
                 // 토큰이 갱실될 때 마다 서버에 토큰을 전달하여 갱신처리를 해야함.
             }
 
+
             override fun onMessageReceived(remoteMessage: RemoteMessage) {
                 // FCM 메시지 수신 시 처리할 코드 작성
                 // 메시지가 서버로부터 전달되었을 때 호출
