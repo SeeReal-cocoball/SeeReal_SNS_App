bba8f8f05ba75f5b21b33186764b72acb430ec2d
ryugeumjung <fbgjung@gmail.com>
2023-05-19 21:32:30 +0900

    test

--- app/src/main/java/com/example/seereal_login/MainActivity.kt
+++ app/src/main/java/com/example/seereal_login/MainActivity.kt
@@ -1,20 +1,14 @@
 package com.example.seereal_login
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.seereal_login.databinding.ActivityMainBinding
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
-import com.google.firebase.messaging.FirebaseMessagingService
-import com.google.firebase.messaging.RemoteMessage
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,45 +29,56 @@ class MainActivity : AppCompatActivity() {
         val firebaseDatabase = FirebaseDatabase.getInstance() // firebase 인스턴스 초기화
         databaseRef = firebaseDatabase.reference // databasereference 초기화
 
+        // users 노드에 접근하고, 사용자 정보를 저장하거나 가져올 수 있다.
+        val usersRef: DatabaseReference = firebaseDatabase.getReference("users")  // users 레퍼런스 가져오기
+
         // 전화번호가 일치하면 비밀번호 입력창으로 넘어간다
         val phone = binding.phone
         val phoneBtn = binding.phonebtn
 
+
         // 입력 버튼을 누르고 회원 존재 여부를 판단한다
         // 회원이 존재 하지 않으면 register화면으로 넘어가는 팝업이 뜬다 => 구현해야 함
         phoneBtn.setOnClickListener() {
+
             // 사용자가 입력한 전화번호
             val phoneTxt = phone.text.toString()
 
             // Phone number를 입력하지 않고 버튼을 눌렀을 경우
             if (phoneTxt.isEmpty()) {
-                Toast.makeText(this, "Please enter your phone number", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Please enter your phone number", Toast.LENGTH_SHORT)
+                    .show()
             }
 
-            val valueEventListener = object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    val databasePhone = dataSnapshot.child("users").child("user1").child("phonenum").getValue(String::class.java)
-                    val databaseNickname = dataSnapshot.child("users").child("user1").child("nickname").getValue(String::class.java)
-
-                    if (databasePhone == phoneTxt) {
-                        Log.d("REAL", "matching success!")
-                        Toast.makeText(this@MainActivity, "Hi $databaseNickname welcome to SeeReal", Toast.LENGTH_SHORT)
-                            .show()
-                        intent.putExtra("nicknametopassword", "${databaseNickname}") // 텍스트 정보 추가
-                        navigateToPasswordPage()
-
-                    } else {
-                        // 일치하는 정보가 데이터베이스에 존재하지 않는 경우
-                        Toast.makeText(this@MainActivity, "Please register", Toast.LENGTH_SHORT)
-                            .show()
+            //val valueEventListener = object : ValueEventListener
+            usersRef.orderByChild("phonenum").equalTo(phoneTxt).addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Hi welcome to SeeReal",
+                                Toast.LENGTH_SHORT).show()
+                            navigateToPasswordPage()
+                        } else {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Phone number not found",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            // FCM 구현해야함
+                        }
                     }
-                }
-                override fun onCancelled(databaseError: DatabaseError) {
-                    // 쿼리 수행이 취소된 경우 발생하는 오류 처리를 수행합니다.
-                }
+                   override fun onCancelled(databaseError: DatabaseError) {
+                        // 에러 처리
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Error: " + databaseError.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                })
             }
-            // ValueEventListener를 등록하여 데이터베이스의 값을 가져옴
-            databaseRef.addListenerForSingleValueEvent(valueEventListener)
         }
     }
-}
+
+
