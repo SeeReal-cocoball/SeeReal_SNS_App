f03eb739277ce3458ba54ec6b35964bfbc3aa8b9
ryugeumjung <fbgjung@gmail.com>
2023-05-23 20:03:55 +0900

    camera

---
+++ app/src/main/java/com/example/seerealcamera/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.seerealcamera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.seerealcamera.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+    }
+}
